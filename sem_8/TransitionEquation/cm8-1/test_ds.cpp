07ad3b6c209c58572dc87034fb6c5c0a02d4aa3d
Muammar Al-Shedivat <muammar.alshedivat@gmail.com>
2016-07-21 21:59:58 +0300

    clang-format sem_8

--- sem_8/TransitionEquation/cm8-1/test_ds.cpp
+++ sem_8/TransitionEquation/cm8-1/test_ds.cpp
@@ -1,71 +1,71 @@
 #include <math.h>
 #include <stdio.h>
 
-#define U(x,t) u[(t-1)*M+x-1]
+#define U(x, t) u[(t - 1) * M + x - 1]
 
-const double Pi = 4.0*atan(1);
+const double Pi = 4.0 * atan(1);
 
 const double a = 0.2;
 
 const int N = 20000;
-const int M = 2000; // x 
+const int M = 2000; // x
 
 // u(t,x) = u(0,x-at)
 
 const double T = 5.0;
 const double A = 3.0;
-double tau = T/double(N);
-double h = A/double(M);
- 
+double tau = T / double(N);
+double h = A / double(M);
 
-double X(int m) { return A*double(m)/double(M); }
-double Xs(int m, int n) { return X(m)+A*T*double(n)/double(N); }
-//double T(int n) { return T*double(n)/double(N); }
+double X(int m) { return A * double(m) / double(M); }
+double Xs(int m, int n) { return X(m) + A * T * double(n) / double(N); }
+// double T(int n) { return T*double(n)/double(N); }
 
-double u_0(double x) { return cos(2.0*Pi*x); }
+double u_0(double x) { return cos(2.0 * Pi * x); }
 
-int main()
-{
-    FILE*F= fopen("data.mp", "w+");
-    fprintf(F, "numeric u; u:=2cm;\n");
-    double* u = new double[M*N];
-    for (int m = 1; m <= M; m++) U(m,1) = u_0(X(m-1));
-    
-    for (int n = 1; n < N; n++)
-    {
-    for (int m = 1; m <= M; m++)
-    {
-        int m1; if (m == M) m1 = 1; else m1 = m+1;
+int main() {
+  FILE *F = fopen("data.mp", "w+");
+  fprintf(F, "numeric u; u:=2cm;\n");
+  double *u = new double[M * N];
+  for (int m = 1; m <= M; m++)
+    U(m, 1) = u_0(X(m - 1));
 
-        U(m,n+1) = a*tau/h*(U(m1,n)-U(m,n)) + U(m,n);
+  for (int n = 1; n < N; n++) {
+    for (int m = 1; m <= M; m++) {
+      int m1;
+      if (m == M)
+        m1 = 1;
+      else
+        m1 = m + 1;
 
+      U(m, n + 1) = a * tau / h * (U(m1, n) - U(m, n)) + U(m, n);
 
-//      fprintf(F, "(%1.4lf, %1.4lf)-- ", X(m), U(m,n));
+      //      fprintf(F, "(%1.4lf, %1.4lf)-- ", X(m), U(m,n));
     }
-//  printf("\n");
-//  printf("%d\n", n);
-    }
-    printf("done\n");
+    //  printf("\n");
+    //  printf("%d\n", n);
+  }
+  printf("done\n");
 
-    double e = 0;
-    for (int m = 1; m <= M; m++)  e+= (fabs(u_0(Xs(m,N)) - U(m,N)))/double(M);
-    printf("Sum error: %1.10lf\n", e);
+  double e = 0;
+  for (int m = 1; m <= M; m++)
+    e += (fabs(u_0(Xs(m, N)) - U(m, N))) / double(M);
+  printf("Sum error: %1.10lf\n", e);
 
-    
-    for (int n = 1; n < N; n+=N/20)
-    {
-        fprintf(F, "\ndraw (%1.3lf*u, %1.3lf*u)\n ", X(0), U(1,n));
-    
-        for (int m = 1; m < M-1; m++) fprintf(F, "--(%1.3lf*u, %1.3lf*u)\n ", X(m), U(m,n));
-        fprintf(F,";\n");
+  for (int n = 1; n < N; n += N / 20) {
+    fprintf(F, "\ndraw (%1.3lf*u, %1.3lf*u)\n ", X(0), U(1, n));
 
+    for (int m = 1; m < M - 1; m++)
+      fprintf(F, "--(%1.3lf*u, %1.3lf*u)\n ", X(m), U(m, n));
+    fprintf(F, ";\n");
 
-//        fprintf(F, "\ndraw (%1.3lf*u, %1.3lf*u)\n ", X(0), u_0(Xs(1,n)));
-    
-//        for (int m = 1; m < M-1; m++) fprintf(F, "--(%1.3lf*u, %1.3lf*u)\n ", X(m), u_0(Xs(m,n)));
-//        fprintf(F," withcolor red;\n");
-    }
-    delete u;
-    
-    return 0;
+    //        fprintf(F, "\ndraw (%1.3lf*u, %1.3lf*u)\n ", X(0), u_0(Xs(1,n)));
+
+    //        for (int m = 1; m < M-1; m++) fprintf(F, "--(%1.3lf*u, %1.3lf*u)\n
+    //        ", X(m), u_0(Xs(m,n)));
+    //        fprintf(F," withcolor red;\n");
+  }
+  delete u;
+
+  return 0;
 }
--- sem_8/TransitionEquation/cm8-2.1/test_ds.cpp
+++ sem_8/TransitionEquation/cm8-2.1/test_ds.cpp
@@ -1,64 +1,75 @@
 #include <math.h>
 #include <stdio.h>
 
-#define U(x,t) u[(t-1)*M+x-1]
+#define U(x, t) u[(t - 1) * M + x - 1]
 
-const double Pi = 4.0*atan(1);
+const double Pi = 4.0 * atan(1);
 
 const double a = 1;
 
 const int N = 5000;
-const int M = 1000; // x 
+const int M = 1000; // x
 
 // u(t,x) = u(0,x-at)
 
 const double T = 5.0;
 const double A = 4.0;
-double tau = T/double(N);
-double h = A/double(M);
- 
+double tau = T / double(N);
+double h = A / double(M);
 
-double X(int m) { return A*double(m)/double(M); }
-//double T(int n) { return T*double(n)/double(N); }
+double X(int m) { return A * double(m) / double(M); }
+// double T(int n) { return T*double(n)/double(N); }
 
-double u_0(double x) { return cos(2.0*Pi*x); }
+double u_0(double x) { return cos(2.0 * Pi * x); }
 
-int main()
-{
-    FILE*F= fopen("data.mp", "w+");
-    fprintf(F, "numeric u; u:=2cm;\n");
-    double* u = new double[M*N];
-    for (int m = 1; m <= M; m++) U(m,1) = u_0(X(m-1));
-    
-    for (int n = 1; n < N; n++)
-    for (int m = 1; m <= M; m++)
-    {
-        int mp1; if (m == M) mp1 = 1; else mp1 = m+1;
-        int mm1; if (m == 1) mm1 = M; else mm1 = m-1;
-        U(m,n+1) = a*tau/(2.0*h)*(U(mp1,n)-U(mm1,n)) + U(m,n);
+int main() {
+  FILE *F = fopen("data.mp", "w+");
+  fprintf(F, "numeric u; u:=2cm;\n");
+  double *u = new double[M * N];
+  for (int m = 1; m <= M; m++)
+    U(m, 1) = u_0(X(m - 1));
+
+  for (int n = 1; n < N; n++)
+    for (int m = 1; m <= M; m++) {
+      int mp1;
+      if (m == M)
+        mp1 = 1;
+      else
+        mp1 = m + 1;
+      int mm1;
+      if (m == 1)
+        mm1 = M;
+      else
+        mm1 = m - 1;
+      U(m, n + 1) = a * tau / (2.0 * h) * (U(mp1, n) - U(mm1, n)) + U(m, n);
     }
-    printf("done\n");
+  printf("done\n");
 
-    double c = 1.7;
-    for (int n = 1; n < N; n+=(N/20))
-    {
+  double c = 1.7;
+  for (int n = 1; n < N; n += (N / 20)) {
     bool suxx = false;
-    double uu0 = U(1,n);
+    double uu0 = U(1, n);
     double av0 = fabs(uu0);
-    if (av0 > c) { uu0 = c*uu0/av0; suxx = true; }
+    if (av0 > c) {
+      uu0 = c * uu0 / av0;
+      suxx = true;
+    }
     fprintf(F, "\ndraw (%1.3lf*u, %1.3lf*u)\n ", X(0), uu0);
-    
-    for (int m = 1; m < M-1; m++)
-    {
-        double uu = U(m,n);
-        double av = fabs(uu);
-        if (av > c) { uu = c*uu/av; suxx  = true; }
-        fprintf(F, "--(%1.3lf*u, %1.3lf*u)\n ", X(m), uu);
+
+    for (int m = 1; m < M - 1; m++) {
+      double uu = U(m, n);
+      double av = fabs(uu);
+      if (av > c) {
+        uu = c * uu / av;
+        suxx = true;
+      }
+      fprintf(F, "--(%1.3lf*u, %1.3lf*u)\n ", X(m), uu);
     }
-    fprintf(F," withpen pencircle scaled .3pt;\n");
-    if (suxx) break; 
-    }
-    delete u;
-    
-    return 0;
+    fprintf(F, " withpen pencircle scaled .3pt;\n");
+    if (suxx)
+      break;
+  }
+  delete u;
+
+  return 0;
 }
